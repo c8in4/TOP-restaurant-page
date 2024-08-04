b058db240a07a6d9b6179472dc0493d78ab57d74
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-08-04 21:42:30 +1200

    Setup webpack
    Webpack uses an html template inside of src/
    Webpack --watch takes that file into account when listening for changes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { watchFile } = require('fs');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  devServer: {
+    watchFiles: ['./src/*.html']
   },
   plugins: [
     new HtmlWebpackPlugin(
