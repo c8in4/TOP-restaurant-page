b1e07f279e539827ae828fd91d929c9cd4047ba6
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-09-29 21:52:36 +1300

    Update webpack.config.js
    Add 'mode: "production"'.

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { watchFile } = require('fs');
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { watchFile } = require("fs")
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "production",
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   devServer: {
-    watchFiles: ['./src/*.html']
+    watchFiles: ["./src/*.html"],
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
-    ]
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
-    new HtmlWebpackPlugin(
-      {
-        template: './src/index.html'
-      }
-    )
-  ]
-};
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+}
