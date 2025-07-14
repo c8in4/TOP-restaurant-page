e2da49a3deb809618cda29183022ff5be284d42a
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-07-14 23:59:15 +1200

    Add functions to switch between tabs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
 import './style.css'
+import homePage from './home';
+import menuPage from './menu';
+import contactPage from './contact';
 
+const contentController = (function () {
+    const mainContainer = document.querySelector('main')
+    const navContainer = document.querySelector('nav')
+
+    let content = homePage()
+
+    updateContent()
+
+    navContainer.addEventListener('click', (event) => {
+        if (event.target.type == 'button') {
+            changeContent(event.target)
+            updateContent()
+        }
+    })
+
+    function changeContent(button) {
+        switch (button.id) {
+            case 'homeButton':
+                console.log('home');
+                content = homePage()
+                break
+            case 'menuButton':
+                console.log('menu');
+                content = menuPage()
+                break
+            case 'contactButton':
+                console.log('contact');
+                content = contactPage()
+                break
+            default:
+                break;
+        }
+    }
+
+    function updateContent() {
+        mainContainer.innerText = ''
+        mainContainer.appendChild(content)
+    }
+})();
