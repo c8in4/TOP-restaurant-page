9e750ff9dfc664376c056a29171ace21f8c4a7d7
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-07-19 20:17:45 +1200

    Release first working and styled version

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,60 @@
-export default function () {
-    const contentDiv = document.createElement('div')
-    contentDiv.id = 'content'
-    contentDiv.classList.add('menu')
+class MenuItem {
+    constructor(name, price, description) {
+        this.name = name
+        this.price = price
+        this.description = description
+    }
+}
 
-    const headLine = document.createElement('h2')
-    headLine.innerText = 'Menu'
+const friedRiceWithTofu = new MenuItem(
+    'with Tofu',
+    '14.50',
+    'Rice fried with vegetables, eggs and tofu.'
+)
+const friedRiceWithChicken = new MenuItem(
+    'with Chicken',
+    '15.50',
+    'Rice fried with vegetables, eggs and chicken.'
+)
+const friedRice = { friedRiceWithTofu, friedRiceWithChicken }
 
-    const paragraph = document.createElement('p')
-    paragraph.innerText = 'test content for menu page'
 
-    contentDiv.append(headLine, paragraph)
+const friedNoodlesWithChicken = new MenuItem(
+    'with Chicken',
+    '15.50',
+    'Egg noodles fried with vegetables, eggs and chicken.'
+)
+const friedNoodlesWithTofu = new MenuItem(
+    'with Tofu',
+    '15.50',
+    'Egg noodles fried with vegetables, eggs and tofu.'
+)
+const noodles = { friedNoodlesWithChicken, friedNoodlesWithTofu }
 
-    return contentDiv
+
+const summerRollsWithTofu = new MenuItem(
+    'Summer Rolls with Tofu',
+    '7.50',
+    'Summer Rolls with Tofu and other stuff.'
+)
+const entrees = { summerRollsWithTofu }
+
+
+const bunderberg = new MenuItem(
+    'Bunderburg',
+    '6',
+    'Gingerbeer, Traditional Lemonade, Tropical Mango'
+)
+const fruitJuice = new MenuItem(
+    'Fruit Juice',
+    '5',
+    'Orange, Apple, Mango, Guave'
+)
+const drinks = { bunderberg, fruitJuice }
+
+export default {
+    'Fried Rice': friedRice,
+    'Fried Noodles': noodles,
+    'Entrees': entrees,
+    'Drinks': drinks
 }
